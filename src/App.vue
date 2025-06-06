093252a7e03589f67912f64d7b2804eba161894f
Maritza <jhoanamrg15@gmail.com>
2025-06-06 16:50:35 -0600

    Mi primer repositorio

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+        <h1>Reporte de Power BI</h1>
+        <p>Visualización interactiva de
+            datos generada en Power BI.</p>
+        <div class="iframe-container">
+
+            <iframe title="visualizacionDatamartNorthwind2" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZWQ0YzFiYmYtNGQ0Yy00OTI1LWI0MGQtMjY2N2EyNWU2ODEzIiwidCI6ImM1Y2Y5NWE2LThlYjgtNDA2NC1hZTUwLTZmOTU0YmFlODU5OCIsImMiOjR9&quot; frameborder= "0 allowFullScreen="true"></iframe>
+       
+        </div>
+    </div>
+  
+</template>
+
