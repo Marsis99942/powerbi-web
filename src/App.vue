fc8690faf8e7fca00b6728fae5dac75c76fded76
Maritza <jhoanamrg15@gmail.com>
2025-06-14 19:05:57 -0600

    Logro: Powerbi-Actualizado

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
             datos generada en Power BI.</p>
         <div class="iframe-container">
 
-            <iframe title="visualizacionDatamartNorthwind2" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZWQ0YzFiYmYtNGQ0Yy00OTI1LWI0MGQtMjY2N2EyNWU2ODEzIiwidCI6ImM1Y2Y5NWE2LThlYjgtNDA2NC1hZTUwLTZmOTU0YmFlODU5OCIsImMiOjR9&quot; frameborder= "0 allowFullScreen="true"></iframe>
+         <iframe title="DATAMART_" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiODA4YjQzMWItMTMxYy00N2NlLWE0ZWMtNjM0NGJjNjc0ODQyIiwidCI6IjIzYWI3OTMzLTA5Y2UtNGM2YS04NGIyLTU5YzZhOTk1Y2Y4YiJ9" frameborder="0" allowFullScreen="true"></iframe>
        
         </div>
     </div>
